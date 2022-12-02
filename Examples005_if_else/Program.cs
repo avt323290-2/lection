5392ee5f2f6dbbbed60b4721d5e271cc551699ec
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-02 15:32:34 +0300

    Добавили способ ввода в разных регистрах

--- Examples005_if_else/Program.cs
+++ Examples005_if_else/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if(username == "Маша")
+if(username == "Маша") // if(username.ToLower() == "Маша") -ввод в верхнем и нижнем регистре.
 {
      Console.WriteLine("Ура, это же Маша! ");
 }
